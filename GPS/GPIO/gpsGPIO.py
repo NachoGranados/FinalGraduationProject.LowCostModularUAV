5ba2b6e88c846941a66a8b19cb02306b0d9cc2d9
Ignacio Granados <ignaciogramar@gmail.com>
2023-10-04 12:06:45 -0600

    Updating GPIO GPS python file

--- GPS/GPIO/gpsGPIO.py
+++ GPS/GPIO/gpsGPIO.py
@@ -1,26 +1,29 @@
+
 # Source: https://www.instructables.com/Interfacing-GPS-Module-With-Raspberry-Pi/
 
 import serial
+import time
+import string
 import pynmea2
 
 while True:
-    
-    port = "/dev/ttyAMAO"
 
-    ser = serial.Serial(port, baudrate = 9600, timeout = 0.5)
+	port = "/dev/ttyAMA0"
 
-    dataout = pynmea2.NMEAStreamReader()
+	ser = serial.Serial(port, baudrate = 9600, timeout = 0.5)
 
-    newdata = ser.readline()
+	dataout = pynmea2.NMEAStreamReader()
 
-    if newdata[0:6] == "$GPRMC":
+	newdata = ser.readline()
 
-        newmsg = pynmea2.parse(newdata)
+	if newdata[0:6] == b"$GNRMC":
 
-        lat = newmsg.latitude
+		newmsg = pynmea2.parse(newdata.decode())
 
-        lng = newmsg.longitude
+		lat = newmsg.latitude
 
-        gps = "Latitude = " + str(lat) + "and Longitude = " + str(lng)
+		lng = newmsg.longitude
 
-        print(gps)
+		gps = "Latitude = " + str(lat) + "and Longitude = " + str(lng)
+
+		print(gps)
