9881819350a8df016972e0f2518dc5915bc968e7
Ignacio Granados <ignaciogramar@gmail.com>
2023-10-04 13:02:30 -0600

    Updating Camera python files

--- Camera/CSI-2/Code/recordVideo.py
+++ Camera/CSI-2/Code/recordVideo.py
@@ -6,7 +6,9 @@ from time import sleep
 camera = PiCamera()
 
 camera.start_preview()
+
 camera.start_recording("video_MIPI_CSI2_Python.h264")
 sleep(5)
 camera.stop_recording()
+
 camera.stop_preview()
--- Camera/CSI-2/Code/takeMultiplePicture.py
+++ Camera/CSI-2/Code/takeMultiplePicture.py
@@ -6,7 +6,9 @@ from time import sleep
 camera = PiCamera()
 
 camera.start_preview()
+
 for i in range(5):
     sleep(5)
     camera.capture("image%s.jpg" % i)
+    
 camera.stop_preview()
